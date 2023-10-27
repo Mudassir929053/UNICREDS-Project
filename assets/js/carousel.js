4f7cbbefd7ecf39962dbc8b842719fbf87c1f840
mudassir929053 <mudassir.intelcode@gmail.com>
2023-10-27 16:36:34 +0530

    Updated Files

---
+++ assets/js/carousel.js
@@ -0,0 +1,209 @@
+(function () {
+
+  // slider first
+
+
+  if ($('#owl-testimonial').length) {
+
+    $('#owl-testimonial').owlCarousel({
+      rtl:true,
+      loop:true,
+      margin:20,
+      nav:true,
+      dots:false,
+      autoplay: true,
+      navText : ["<i class='fa fa-chevron-right'></i>","<i class='fa fa-chevron-left'></i>"],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:3
+          }
+      }
+  })
+  }
+
+  if ($('#owl-testimonial-second').length) {
+
+    $('#owl-testimonial-second').owlCarousel({
+      rtl:true,
+      loop:true,
+      margin:0,
+      nav:true,
+      dots:false,
+      autoplay: true,
+       navText : ["<i class='fa fa-chevron-right'></i>","<i class='fa fa-chevron-left'></i>"],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
+  })
+  }
+
+
+  if ($('#owl-slider').length) {
+
+    $('#owl-slider').owlCarousel({
+      rtl:true,
+      loop:true,
+      margin:20,
+      nav:true,
+      dots:false,
+      autoplay: true,
+       navText : ["<i class='fa fa-chevron-right'></i>","<i class='fa fa-chevron-left'></i>"],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:4
+          }
+      }
+  })
+  }
+
+
+  if ($('#owl-slider-two').length) {
+
+    $('#owl-slider-two').owlCarousel({
+      rtl:true,
+      loop:true,
+      margin:20,
+      nav:true,
+      dots:false,
+      autoplay: true,
+       navText : ["<i class='fa fa-chevron-right'></i>","<i class='fa fa-chevron-left'></i>"],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:4
+          }
+      }
+  })
+  }
+  if ($('#owl-slider-three').length) {
+
+    $('#owl-slider-three').owlCarousel({
+      rtl:true,
+      loop:true,
+      margin:20,
+      nav:true,
+      dots:false,
+      autoplay: true,
+       navText : ["<i class='fa fa-chevron-right'></i>","<i class='fa fa-chevron-left'></i>"],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:4
+          }
+      }
+  })
+  }
+
+  if ($('#owl-slider-four').length) {
+
+    $('#owl-slider-four').owlCarousel({
+      rtl:true,
+      loop:true,
+      margin:20,
+      nav:true,
+      dots:false,
+      autoplay: true,
+       navText : ["<i class='fa fa-chevron-right'></i>","<i class='fa fa-chevron-left'></i>"],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:4
+          }
+      }
+  })
+  }
+
+
+
+  if ($('#owl-slider-five').length) {
+
+    $('#owl-slider-five').owlCarousel({
+      rtl:true,
+      loop:true,
+      margin:20,
+      nav:true,
+      dots:false,
+      autoplay: true,
+       navText : ["<i class='fa fa-chevron-right'></i>","<i class='fa fa-chevron-left'></i>"],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:4
+          }
+      }
+  })
+  }
+
+
+  if ($('#owl-slider-six').length) {
+
+    $('#owl-slider-six').owlCarousel({
+      rtl:true,
+      loop:true,
+      margin:20,
+      nav:true,
+      dots:false,
+      autoplay: true,
+       navText : ["<i class='fa fa-chevron-right'></i>","<i class='fa fa-chevron-left'></i>"],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:4
+          }
+      }
+  })
+  }
+
+
+
+
+
+
+
+
+})();
